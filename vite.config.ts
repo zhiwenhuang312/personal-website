046c9d1a84d3ba8f251d3e855e33b594c2373fff
zhiwenhuang312 <zhiwenhuang312@gmail.com>
2026-06-04 18:37:17 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/prismatic-portrayal/",
+  base: "/personal-website/",
   plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
